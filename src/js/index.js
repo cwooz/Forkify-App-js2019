a9f3c9a16c73ed981e1b55b8fb0198b81ec7592a
cwooz <woosley.christopher@gmail.com>
2019-08-01 19:39:00 -0400

    search view

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,11 @@
 // https://www.food2fork.com/api/search
 // https://www.food2fork.com/api/get 
-// e7ad8e1dcb4babf119f92615c3d9faf1
+// api_key = e7ad8e1dcb4babf119f92615c3d9faf1
 
 
 import Search from "./models/Search";
+import * as searchView from './views/searchView';
+import { elements } from "./views/base";
 
 /** 
  * Global State of Application
@@ -16,23 +18,25 @@ const state = {}
 
 const controlSearch = async () => {
   // 1) Get query from view
-  const query = 'pizza';  // TODO!
+  const query = searchView.getInput();
   
   if (query) {
     // 2) New search object and add to state
     state.search = new Search(query);
 
     // 3) Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
 
     // 4) Search for recipes
     await state.search.getResults();
 
     // 5) Render results in UI
-    console.log(state.search.result);
+    searchView.renderResults(state.search.result);
   }
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
   e.preventDefault();
   controlSearch();
 });
