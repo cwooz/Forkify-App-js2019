5766a2b918f39c30691e606d5b6551ea60acaa3a
cwooz <woosley.christopher@gmail.com>
2019-08-01 13:36:32 -0400

    start API calls

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
-// Global app controller
-import string from "./models/Search";
-import { add, mult, ID } from "./views/searchView";
+// // Global app controller
+// import string from "./models/Search";
+// // import { add, mult, ID } from "./views/searchView";
+// import * as searchView from "./views/searchView";
 
-console.log(`Using imported functions! ${add(ID, 2)} and ${mult(ID, 3)}`);
+// console.log(`Using imported functions! ${searchView.add(searchView.ID, 2)} and ${searchView.mult(searchView.ID, 3)}`);
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,1 @@
-export default "I am an exported string.";
+// export default "I am an exported string.";
