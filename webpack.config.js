4863a1089ce4125cf92b041a83c718e1293d3468
cwooz <woosley.christopher@gmail.com>
2019-08-01 11:03:29 -0400

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+// Webpack Core Concepts: 1 Entry Point, 2 Output, 3 Plug-ins, 4 Loaders
+
 module.exports = {
     entry: ['@babel/polyfill', './src/js/index.js'],
     output: {
