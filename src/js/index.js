c1634a361893f672b59e453a270fa6c17c690a98
cwooz <woosley.christopher@gmail.com>
2019-08-01 13:31:20 -0400

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,5 @@
 // Global app controller
+import string from "./models/Search";
+import { add, mult, ID } from "./views/searchView";
+
+console.log(`Using imported functions! ${add(ID, 2)} and ${mult(ID, 3)}`);
